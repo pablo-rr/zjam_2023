b0614a9425cba73a42d9f8bfa66a967a59a060e9
pablo-rr <pablorr2@gmail.com>
2023-05-05 23:44:11 +0200

    Testing purposes
    Signed-off-by: pablo-rr <pablorr2@gmail.com>

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
 
 
-@export var speed : float = 300.0
+@export var max_speed : float = 300.0
 @export var jump_force : float = -400.0
 @export var jump_force_extra_fill : float = 500.0
 @export var minimum_charge_time : float = 0.75
+@export var acceleration : float = 100.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var jump_force_extra : float = 0.0
 @onready var time_charging_jump : float = 0.0
-@onready var charging_energy : bool = true
+@onready var charging_energy : bool = false
+@onready var acceleration_modifier : float = 0.0
+@onready var last_direction : float = 0.0
+@onready var speed : float = 0.0
+@onready var energy_to_regen : float = 0.0
 
 func _input(event: InputEvent) -> void:
 	if(Input.is_action_pressed("ui_charge_energy") and is_on_floor()):
@@ -24,38 +29,45 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle Jump.
-	
-	print("time_charging_jump", str(time_charging_jump))
-	print("jump_force_extra", str(jump_force_extra))
-	
+	if(Input.is_action_just_pressed("ui_jump") and is_on_floor()):
+		energy_to_regen = 0.0
+		velocity.y = jump_force - jump_force_extra
+		jump_force_extra = delta * jump_force_extra_fill
+		charging_energy = false
+
 	if(charging_energy):
+		$StaminaSystem.waste(delta)
 		time_charging_jump += delta
 		if(time_charging_jump >= minimum_charge_time):
 			jump_force_extra += delta * jump_force_extra_fill
 	else:
+		$StaminaSystem.add_stamina(energy_to_regen)
+		energy_to_regen = 0.0
 		time_charging_jump = 0.0
 		jump_force_extra = 0.0
 		
-	if(Input.is_action_just_pressed("ui_jump") and is_on_floor()):
-		velocity.y = jump_force - jump_force_extra
-		jump_force_extra = delta * jump_force_extra_fill
-		charging_energy = false
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * speed
+#		velocity.x = direction * max_speed * acceleration_modifier
 		$AnimationPlayer.play("Walk")
 		if(direction >= 0): 
 			$Sprite2D.flip_h = false
 		else:
 			$Sprite2D.flip_h = true
+			
+		last_direction = direction
+		speed = move_toward(speed, max_speed * last_direction, acceleration)
 	else:
 		$AnimationPlayer.play("Idle")
-		velocity.x = move_toward(velocity.x, 0, speed)
+		speed = move_toward(speed, 0.0, acceleration)
 		
+	velocity.x = speed
 	
 
 	move_and_slide()
+
+func _on_stamina_system_wasted(ammount) -> void:
+	energy_to_regen += ammount
