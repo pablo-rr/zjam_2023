06d60cff3b548a31b579813bb90824eb6bd0cdb7
pablo-rr <pablorr2@gmail.com>
2023-05-06 12:49:00 +0200

    Parallax
    Signed-off-by: pablo-rr <pablorr2@gmail.com>

---
+++ Scenes/Scenery/parallax_background.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$Clouds.motion_offset.x -= delta * 10
+	$Fog.motion_offset.x -= delta * 25
