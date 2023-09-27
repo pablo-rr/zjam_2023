56955b11b724166feedcccff4e904a351ba305a4
pablo-romero <pablo.romero@bixel.es>
2023-09-27 17:28:28 +0200

    Portal polishing
    Signed-off-by: pablo-romero <pablo.romero@bixel.es>

--- Addons/Camera/Camera2DFollowNode.gd
+++ Addons/Camera/Camera2DFollowNode.gd
@@ -3,6 +3,9 @@ extends Camera2D
 
 @export var target : Node2D
 @export var speed : float = 0.09
+@export var max_movement_offset : float = 35.0
+
+var movement_offset : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,9 +14,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position = lerp(position, target.position, speed)
-#	position = target.position
+	var offset_position = target.position
+	offset_position.x += movement_offset
+	
+	position = lerp(position, offset_position, speed)
 	if(target.infinite_energy):
 		$AnimationPlayer.play("Shake")
 	else:
 		$AnimationPlayer.play("RESET")
+		
+	if(target.speed > 0):
+		movement_offset = move_toward(movement_offset, max_movement_offset, 5)
+	elif(target.speed < 0):
+		movement_offset = move_toward(movement_offset, -max_movement_offset, 5)
+#	else:
+#		movement_offset = lerp(movement_offset, 0.0, 0.06)
